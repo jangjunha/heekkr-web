f73423645e1e004960d62c2a5b983c6087fb33ef
jangjunha <hi@jangjunha.me>
2023-08-22 12:16:05 +0900

    feat: add library loading indicator

--- src/components/LibraryFilter.tsx
+++ src/components/LibraryFilter.tsx
@@ -4,6 +4,7 @@ import { useEffect, useMemo, useState } from "react";
 import { maps } from "..";
 import { getLibraries } from "../api";
 import { useDebounce } from "usehooks-ts";
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
 
 interface LibraryFilter {
   value?: string[];
@@ -19,7 +20,7 @@ const LibraryFilter = ({
   const [currentPosition, setCurrentPosition] = useState<GeolocationPosition>();
 
   const [keyword, setKeyword] = useState<string>("");
-  const [libraries, setLibraries] = useState<Library[]>([]);
+  const [libraries, setLibraries] = useState<Library[]>();
 
   useEffect(() => {
     const loadAsync = async () => {
@@ -46,7 +47,7 @@ const LibraryFilter = ({
     if (currentPosition == null || spherical == null) {
       return [];
     }
-    return libraries
+    return (libraries ?? [])
       .map((lib): [string, number] => {
         const coord = lib.getCoordinate();
         if (coord == null) {
@@ -73,7 +74,7 @@ const LibraryFilter = ({
   const sortedLibraries = useMemo(
     () =>
       isKeywordEmpty
-        ? libraries.concat().sort((a, b) => {
+        ? libraries?.concat().sort((a, b) => {
             const distA =
               distances.findIndex(([id]) => id == a.getId()) ?? Infinity;
             const distB =
@@ -108,40 +109,49 @@ const LibraryFilter = ({
             className="py-0 border-slate-100 shadow-sm rounded-md"
           />
         </div>
-        <div className="flex items-center gap-2 [&>button]:bg-blue-600 [&>button]:px-2 [&>button]:rounded-md [&>button]:text-sm [&>button]:text-white">
-          <button onClick={() => selectByDistance(500)}>500m</button>
-          <button onClick={() => selectByDistance(1000)}>1km</button>
-          <button onClick={() => selectByDistance(1500)}>1.5km</button>
-          <button onClick={() => selectByDistance(3000)}>3km</button>
-        </div>
+        {currentPosition && (
+          <div className="flex items-center gap-2 [&>button]:bg-blue-600 [&>button]:px-2 [&>button]:rounded-md [&>button]:text-sm [&>button]:text-white">
+            <button onClick={() => selectByDistance(500)}>500m</button>
+            <button onClick={() => selectByDistance(1000)}>1km</button>
+            <button onClick={() => selectByDistance(1500)}>1.5km</button>
+            <button onClick={() => selectByDistance(3000)}>3km</button>
+          </div>
+        )}
       </div>
-      <ul
-        role="list"
-        className="grid lg:grid-cols-4 md:grid-cols-3 gap-x-4 max-h-[5.2em] overflow-scroll"
-      >
-        {sortedLibraries.map((lib) => (
-          <li key={lib.getId()}>
-            <label>
-              <input
-                type="checkbox"
-                checked={value.includes(lib.getId())}
-                onChange={(e): void => {
-                  if (e.currentTarget.checked) {
-                    if (value.length >= 10) {
-                      return;
+      {sortedLibraries == null ? (
+        <div className="flex gap-2 justify-center items-center py-4 text-slate-500">
+          <ArrowPathIcon className="animate-spin h-6 w-6" />
+          <p>불러오는 중</p>
+        </div>
+      ) : (
+        <ul
+          role="list"
+          className="grid lg:grid-cols-4 md:grid-cols-3 gap-x-4 max-h-[5.2em] overflow-scroll"
+        >
+          {sortedLibraries.map((lib) => (
+            <li key={lib.getId()}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={value.includes(lib.getId())}
+                  onChange={(e): void => {
+                    if (e.currentTarget.checked) {
+                      if (value.length >= 10) {
+                        return;
+                      }
+                      onChange?.([...value, lib.getId()]);
+                    } else {
+                      onChange?.(value.filter((l) => l != lib.getId()));
                     }
-                    onChange?.([...value, lib.getId()]);
-                  } else {
-                    onChange?.(value.filter((l) => l != lib.getId()));
-                  }
-                }}
-                className="mr-1 border-slate-300 rounded-md"
-              />
-              {lib.getName()}
-            </label>
-          </li>
-        ))}
-      </ul>
+                  }}
+                  className="mr-1 border-slate-300 rounded-md"
+                />
+                {lib.getName()}
+              </label>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
